99737eb6937304d03f8343addd5424ff99222b47
pedrohqb <60863081+pedrohqb@users.noreply.github.com>
2025-01-01 17:30:26 -0300

    Add files via upload

---
+++ nixos/home.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz";
+  };
+in
+{
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  home-manager.users.pedro = {
+    # This should be the same value as `system.stateVersion` in
+    # your `configuration.nix` file.
+    home.stateVersion = "24.11";
+
+    home.file = {
+      
+      # Add Calibri font to Evolution
+      ".local/share/evolution/webkit-editor-plugins/body-font.js" = {
+        text = ''
+          'use strict';
+
+          var localhostBodyFontPlugin = {
+             name : "localhostBodyFontPlugin",
+             setup : function(doc) {
+                if (doc.body) {
+                    doc.body.setAttribute("style", "font-family: Calibri,sans-serif; font-size: 11.0pt;")
+                }
+             }
+          };
+
+          EvoEditor.RegisterPlugin(localhostBodyFontPlugin);
+        '';
+      };        
+    };
+  };
+}
